bc1716cff3682d0417db2eb1ebf193f3ef34decd
robotoss <zoxo@outlook.com>
2020-12-31 00:56:59 +0600

    ReInstall project

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:aqueduct_pd/aqueduct_pd.dart';
+
+Future main() async {
+  final app = Application<AqueductPdChannel>()
+      ..options.configurationFilePath = "config.yaml"
+      ..options.port = 8888;
+
+  await app.startOnCurrentIsolate();
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ lib/aqueduct_pd.dart
@@ -0,0 +1,11 @@
+/// aqueduct_pd
+///
+/// A Aqueduct web server.
+library aqueduct_pd;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
