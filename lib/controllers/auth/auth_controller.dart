ebf4368dd54f88ce32b25d96fa062141815853fc
robotoss <zoxo@outlook.com>
2021-01-10 19:22:06 +0600

    Add test auth

--- lib/controllers/auth/auth_controller.dart
+++ lib/controllers/auth/auth_controller.dart
@@ -20,69 +20,74 @@ class AuthenticationController extends ResourceController {
       return Response.conflict(body: {"error": "Заполните данные"});
     }
 
-    final googleData = await GoogleHelper().getUserData(mobileToken);
-
-    if (googleData?.users != null) {
-      User userData;
-      try {
-        userData = await _dataBase
-            .queryPhoneNumber(googleData.users.first.phoneNumber);
-      } catch (e) {}
-
-      if (userData == null) {
-        // Создаю нового пользователя
-        await Database(context).registrationUserByPhone(
-          googleData.users.first.phoneNumber,
-          3,
-        );
-      }
-      userData =
-          await _dataBase.queryPhoneNumber(googleData.users.first.phoneNumber);
-      // Получаю токен авторизации
-      final token = SecurityHelper().signToken(userData.id);
-
-      return Response.ok(
-        {
-          "auth_token": token,
-          "user_data": {
-            "id": userData.id,
-            "role": {
-              "id": userData.userRole.id,
-              "name_ru": userData.userRole.nameRu,
-              "name_en": userData.userRole.nameEn,
-            },
-            "name": userData.fullName,
-            "address": userData.address,
-            "phone_number": userData.phoneNumber,
-            "img": userData.img,
-            "email": userData.email,
-          },
-        },
-      );
+    User userData;
+    String phoneNumber;
+    //  Проверяю тестовый ли это токен
+    if (mobileToken ==
+        'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYxMDE3NzY2NSwiaWF0IjoxNjEwMTc3NjY1fQ.QL8h_oNeH4k4jqb7QBU5eEJM-zHZ6Pqks7BNDgPhsuo') {
+      phoneNumber = '+77990000000';
     } else {
-      return Response.serverError(body: {"error": "Произошла ошибка"});
-    }
-  }
+      final googleData = await GoogleHelper().getUserData(mobileToken);
 
-  @override
-  Map<String, APIResponse> documentOperationResponses(
-      APIDocumentContext context, Operation operation) {
-    if (operation.method == "POST") {
-      return {
-        "200": APIResponse.schema(
-          "AuthRepository",
-          context.schema.getObjectWithType(AuthRepository),
-        ),
-        "500": APIResponse.schema(
-          "AuthRepositoryError",
-          context.schema["AuthRepositoryError"],
-        ),
-        "409": APIResponse.schema(
-          "AuthRepositoryError",
-          context.schema["AuthRepositoryError"],
-        )
-      };
+      if (googleData?.users == null) {
+        return Response.serverError(body: {"error": "Произошла ошибка"});
+      } else {
+        phoneNumber = googleData.users.first.phoneNumber;
+      }
     }
-    return null;
+    // Пытаюсь найти пользователя в базе данных
+    userData = await _dataBase.queryPhoneNumber(phoneNumber);
+
+    if (userData == null) {
+      // Создаю нового пользователя если данные не найдены
+      await Database(context).registrationUserByPhone(
+        phoneNumber,
+        3,
+      );
+    }
+    userData = await _dataBase.queryPhoneNumber(phoneNumber);
+    // Получаю токен авторизации
+    final token = SecurityHelper().signToken(userData.id);
+
+    return Response.ok(
+      {
+        "auth_token": token,
+        "user_data": {
+          "id": userData.id,
+          "role": {
+            "id": userData.userRole.id,
+            "name_ru": userData.userRole.nameRu,
+            "name_en": userData.userRole.nameEn,
+          },
+          "name": userData.fullName,
+          "address": userData.address,
+          "phone_number": userData.phoneNumber,
+          "img": userData.img,
+          "email": userData.email,
+        },
+      },
+    );
   }
+}
+
+@override
+Map<String, APIResponse> documentOperationResponses(
+    APIDocumentContext context, Operation operation) {
+  if (operation.method == "POST") {
+    return {
+      "200": APIResponse.schema(
+        "AuthRepository",
+        context.schema.getObjectWithType(AuthRepository),
+      ),
+      "500": APIResponse.schema(
+        "AuthRepositoryError",
+        context.schema["AuthRepositoryError"],
+      ),
+      "409": APIResponse.schema(
+        "AuthRepositoryError",
+        context.schema["AuthRepositoryError"],
+      )
+    };
+  }
+  return null;
 }
