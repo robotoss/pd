582c169f0869b934393a1b20f52f67094dfdc9e8
robotoss <zoxo@outlook.com>
2021-01-11 13:36:46 +0600

    clean code

--- lib/models/request/user/user_model.dart
+++ lib/models/request/user/user_model.dart
@@ -1,33 +1,3 @@
-// class UserRequst {
-//   UserRequst(this.userName, this.phoneNumber, this.city, this.country);
-
-//   factory UserRequst.fromJson(Map<String, dynamic> json) {
-//     if (json == null) {
-//       throw const FormatException('Null JSON in User constructor');
-//     }
-//     return UserRequst(
-//       json['userName'] as String,
-//       json['phoneNumber'] as String,
-//       json['city'] as String,
-//       json['country'] as String,
-//     );
-//   }
-
-//   String userName;
-//   String phoneNumber;
-//   String city;
-//   String country;
-
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'userName': userName,
-//       'phoneNumber': phoneNumber,
-//       'city': city,
-//       'country': country,
-//     };
-//   }
-// }
-
 import 'package:aqueduct_pd/aqueduct_pd.dart';
 
 class UserRequst extends Serializable{
